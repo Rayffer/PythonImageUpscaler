748b91862502805ec1cec8ff680de26e892b166a
Oscar Guillamon Rojo <oscar.guillamon_rojo@roche.com>
2021-07-21 15:30:03 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,25 @@
+# Python Image Upscaler
+
+**PythonImageUpscaler** is a project that uses opencv super resolution functionality along with some deep learning algorithms to enhance image quality when upscaling.
+
+The deep learning algorithms used are available at the following repositories:
+
+- [EDSR in Tensorflow](https://github.com/Saafke/EDSR_Tensorflow)
+- [ESPCN in Tensorflow](https://github.com/fannymonori/TF-ESPCN)
+- [FSRCNN in Tensorflow](https://github.com/Saafke/FSRCNN_Tensorflow)
+- [LapSRN in Tensorflow](https://github.com/fannymonori/TF-LapSRN)
+
+## Dependencies
+
+This project is based on python 3.8.5, and uses opencv for image processing and if requested for interactive visualization of results
+
+## Executing
+
+The python script accepts 4 arguments:
+
+- **filepath:** The filepath to the image desired to upscale.
+- **algorithm:** The algorithm to use for image upscaling (f.e. EDSR_x2).
+- **outputfilepath:** The filepath where the image will be saved.
+- **showresults:** Whether to show a window with the result to interactively zoom to inspect them, optional.
+
+The parameter **Algorithm** only accepts a collection of values, too see the available ones, use the `--help` command."# PythonImageUpscaler" 
---
+++ main.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import cv2
+import sys
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process an image specified to upscale it via DL algorithms.")
+    parser.add_argument("--filepath", required=True, help="The image filepath to upscale", type=str)
+    parser.add_argument("--algorithm", required=True, help="the DL algorithm to use", type=str, choices=['EDSR_x2', "EDSR_x3", "EDSR_x4", "ESPCN_x2", "ESPCN_x3", "ESPCN_x4", "FSRCNN_x2", "FSRCNN_x3", "FSRCNN_x4", "LapSRN_x2", "LapSRN_x4", "LapSRN_x8"])
+    parser.add_argument("--outputfilepath", required=True, help="The path in which to save the result", type=str)
+    parser.add_argument("--showresults", required=False, help="Whether to show the image for comparison", type=bool)
+    try:
+        args = parser.parse_args()
+    except argparse.ArgumentTypeError:
+        sys.exit()
+
+    image = cv2.imread(args.filepath)
+
+
+    super_resolution_algorithm = cv2.dnn_superres.DnnSuperResImpl_create()
+
+    path = f"Models\{args.algorithm}.pb"
+
+    super_resolution_algorithm.readModel(path)
+    
+    algorithm_arguments = args.algorithm.split('_x')
+    
+    super_resolution_algorithm.setModel(algorithm_arguments[0].lower(),int(algorithm_arguments[1]))
+
+    result = super_resolution_algorithm.upsample(image)
+
+    cv2.imwrite(args.outputfilepath, result)
+
+    if parser.showresults:
+        # Resized image
+        resized = cv2.resize(image,dsize=None,fx=int(algorithm_arguments[1]),fy=int(algorithm_arguments[1]))
+        plt.figure(figsize=(12,8))
+        plt.subplot(1,3,1)
+        # Original image
+        plt.imshow(image[:,:,::-1])
+        plt.subplot(1,3,2)
+        # SR upscaled
+        plt.imshow(result[:,:,::-1])
+        plt.subplot(1,3,3)
+
+        # OpenCV upscaled
+        plt.imshow(resized[:,:,::-1])
+        plt.show()  
